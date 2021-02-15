7692fe3f68ba765a3d37d97653ab3467a3273913
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-02-15 12:57:54 -0600

    Init commit

---
+++ src/main/java/com/bluemooses/oysterweb/OysterWebApplication.java
@@ -0,0 +1,13 @@
+package com.bluemooses.oysterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OysterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OysterWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluemooses/oysterweb/OysterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluemooses.oysterweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OysterWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
